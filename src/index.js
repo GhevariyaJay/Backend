e89885964ddcdbd6de362ca9481074653ac72cc3
Jay Ghevariya <jayghevariya1@gmail.com>
2024-05-30 16:03:55 +0530

    add changes

--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ dotenv.config({
 })
 
 connectDB()
+.then( () => {
+    app.listen(process.env.PORT || 3000 , () => {
+        console.log(`Server is listening on ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGODB connection failed !!", err)
+})
 
 
 
